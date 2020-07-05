f25e9ca846d4cc4382c99445a62d69b8fe93f749
yuvi1947 <yuvi1947@users.noreply.github.com>
2020-07-05 20:01:55 +0530

    Adding swap to Digital ocean server

--- all.sh
+++ all.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
-
 echo "
 ----------------------
   NODE & NPM
 ----------------------
 "
-
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 # install nodejs and npm
 sudo apt-get install -y nodejs
@@ -15,7 +13,6 @@ echo "
   MONGODB
 ----------------------
 "
-
 # import key
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 # update sources.list
@@ -34,7 +31,6 @@ echo "
   PM2
 ----------------------
 "
-
 # install pm2 with npm
 sudo npm install -g pm2
 # set pm2 to start automatically on system startup
@@ -45,7 +41,6 @@ echo "
   NGINX
 ----------------------
 "
-
 # install nginx
 sudo apt-get install -y nginx
 
@@ -55,10 +50,42 @@ echo "
   UFW (FIREWALL)
 ----------------------
 "
-
 # allow ssh connections through firewall
 sudo ufw allow OpenSSH
 # allow http & https through firewall
 sudo ufw allow 'Nginx Full'
 # enable firewall
 sudo ufw --force enable
+
+
+echo "
+----------------------
+  CREATE AND MAINTAIN SWAP
+----------------------
+"
+#checking if swap exists
+sudo swapon --show
+free -h
+
+#Creating a swap
+sudo fallocate -l 1G /swapfile
+ls -lh /swapfile
+sudo chmod 600 /swapfile
+ls -lh /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo swapon --show
+
+#Making the swap permanent
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+#Tuning swap setting for SSD based servers
+sudo sysctl vm.swappiness=10
+sudo sysctl vm.vfs_cache_pressure=50
+
+#Making the swap setttings permanent
+sudo cp /etc/sysctl.conf /etc/sysctl.conf.bak
+echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
+echo 'vm.vfs_cache_pressure = 50' | sudo tee -a /etc/sysctl.conf
+
